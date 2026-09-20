0166e25f51d43d9ab6fbcaf72fbf787f496d28be
pkj-web <130211933+pkj-web@users.noreply.github.com>
2026-09-20 17:46:15 -0400

    Add: string-encode-and-decode - submission-19

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-19.py
@@ -0,0 +1,39 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        res = []
+
+        for i in strs:
+            res.append(str(len(i)))
+            res.append("#")
+            res.append(i)
+
+        return "".join(res)
+
+    def decode(self, s: str) -> List[str]:
+
+        geeg = []
+
+
+        i=0
+
+
+        while i < len(s):
+            j=i
+
+            while s[j] != "#":
+                j+=1
+
+            length = int(s[i:j])
+
+            i = j+1
+
+            j = length + i
+
+            geeg.append(s[i:j])
+
+            i=j
+        return geeg
+
+
