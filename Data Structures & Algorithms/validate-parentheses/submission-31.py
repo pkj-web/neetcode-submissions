93bc968b143b8dacf3105bc4485902c2e2cce9a4
pkj-web <130211933+pkj-web@users.noreply.github.com>
2026-09-21 00:51:55 -0400

    Add: validate-parentheses - submission-31

---
+++ Data Structures & Algorithms/validate-parentheses/submission-31.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        stack = []
+
+        valid = { "}": "{", "]":"[", ")" : "(" }
+
+        for i in s:
+
+            if i in valid:
+                if stack and stack[-1]==valid[i]:
+                    stack.pop()
+
+                else:
+                    return False
+
+
+
+            else:
+                stack.append(i)
+
+
+        return True if not stack else False
+        
+
+
+
+
